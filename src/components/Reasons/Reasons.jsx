3930a547d1e441bae9bd1f996ea2a5efdab1f947
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-07-04 23:05:45 +0200

    reasosn2

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -11,8 +11,39 @@ const Reasons = () => {
             <img src="image4.png" alt="" />
         </div>
          <div className="right-r">
-            right
-         </div>
+          <span>some reasons</span>
+          <div>
+            <span className='stroke'>Why</span>
+            <span> Choose us</span>
+          </div>
+
+            <div className='details-r'>
+            <div><img src="tick.png" alt=""></img>
+            <span> OVER 140+ EXPERT COACH</span>
+            </div>
+            <div><img src="tick.png" alt="" />
+            <span>TRAIN SMARTER</span>
+            </div>
+            <div><img src="tick.png" alt="" />
+            <span>1 FREE PROGRAM</span>
+            </div>
+            <div><img src="tick.png" alt="" />
+            <span>RELIABLE PATNERS</span>
+            </div>
+            </div>
+            <span style={{
+              color:'gray',
+              fonWeight:'normal'
+
+            }}>
+              OURS PATNERS
+            </span>
+            <div className="partners">
+              <img src="nb.png" alt="" />
+              <img src="nike.png" alt="" />
+              <img src="adidas.png" alt="" />
+              </div>
+          </div>
     </div>
   )
 }
